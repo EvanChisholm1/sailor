7df3e3563f827b9fe1216f081411e7ecd0d129e0
Evan Chisholm <elchisholm248@gmail.com>
2023-09-04 17:53:38 -0400

    added list of all embeddings and a search endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 app = Flask(__name__)
 
@@ -23,18 +24,52 @@ def getTextChunks(text):
     
     return sentences
 
+embeddings = []
 
 @app.route("/", methods=["GET", "POST"])
 @cross_origin()
 def main():
     if request.method == 'POST':
+        print("post method")
         print(f"URL: {request.json['url']}\n")
 
         for sentence in getTextChunks(request.json['text']):
-            print(sentence, model.encode(sentence), '\n')
-            print()
-        # print(model.encode(request.json['text']))
-        print("post method")
+            embeddings.append({
+                'url': request.json['url'], 
+                'sentence': sentence, 
+                'embedding': model.encode(sentence)
+            })
+
+        for e in embeddings:
+            print(e['url'], "\n")
+
+        return '{"message": "embedded %s"}' % request.json['url']
+            
 
     return '{"message": "Hello world"}'
 
+
+@app.get("/search")
+@cross_origin()
+def search():
+    query = request.args.get('q')
+
+    print("searched for:", query)
+
+    embededQ = model.encode(query)
+    most_similar = {}
+    highest_similarity = 0
+
+    for e in embeddings:
+        dot_product = np.dot(e['embedding'], embededQ)
+        normA = np.linalg.norm(e['embedding'])
+        normB = np.linalg.norm(embededQ)
+        sim = dot_product / (normA * normB);
+
+        if sim > highest_similarity:
+            most_similar = e
+            highest_similarity = sim
+        
+    print(most_similar)
+
+    return "..."
